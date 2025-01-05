54183dff102eeda3fd35e94018a7555f038c641d
TheDeveloperEver <levilouis@protonmail.com>
2025-01-04 21:52:44 -0500

    Change to MMC Velocity

--- spigotv-server/src/main/java/net/minecraft/server/PacketPlayOutEntityVelocity.java
+++ spigotv-server/src/main/java/net/minecraft/server/PacketPlayOutEntityVelocity.java
@@ -1,8 +1,13 @@
 package net.minecraft.server;
 
 import java.io.IOException;
+import net.minecraft.server.Entity;
+import net.minecraft.server.Packet;
+import net.minecraft.server.PacketDataSerializer;
+import net.minecraft.server.PacketListenerPlayOut;
 
-public class PacketPlayOutEntityVelocity implements Packet<PacketListenerPlayOut> {
+public class PacketPlayOutEntityVelocity
+implements Packet<PacketListenerPlayOut> {
     private int a;
     private int b;
     private int c;
@@ -17,36 +22,31 @@ public class PacketPlayOutEntityVelocity implements Packet<PacketListenerPlayOut
 
     public PacketPlayOutEntityVelocity(int var1, double var2, double var4, double var6) {
         this.a = var1;
-        double var8 = 3.9D;
+        double var8 = 3.9;
         if (var2 < -var8) {
             var2 = -var8;
         }
-
         if (var4 < -var8) {
             var4 = -var8;
         }
-
         if (var6 < -var8) {
             var6 = -var8;
         }
-
         if (var2 > var8) {
             var2 = var8;
         }
-
         if (var4 > var8) {
             var4 = var8;
         }
-
         if (var6 > var8) {
             var6 = var8;
         }
-
-        this.b = (int) (var2 * 8000.0D);
-        this.c = (int) (var4 * 8000.0D);
-        this.d = (int) (var6 * 8000.0D);
+        this.b = (int)(var2 * 8000.0);
+        this.c = (int)(var4 * 8000.0);
+        this.d = (int)(var6 * 8000.0);
     }
 
+    @Override
     public void a(PacketDataSerializer var1) throws IOException {
         this.a = var1.e();
         this.b = var1.readShort();
@@ -54,6 +54,7 @@ public class PacketPlayOutEntityVelocity implements Packet<PacketListenerPlayOut
         this.d = var1.readShort();
     }
 
+    @Override
     public void b(PacketDataSerializer var1) throws IOException {
         var1.b(this.a);
         var1.writeShort(this.b);
@@ -61,23 +62,25 @@ public class PacketPlayOutEntityVelocity implements Packet<PacketListenerPlayOut
         var1.writeShort(this.d);
     }
 
+    @Override
     public void a(PacketListenerPlayOut var1) {
         var1.a(this);
     }
 
     public int getA() {
-        return a;
+        return this.a;
     }
 
     public int getB() {
-        return b;
+        return this.b;
     }
 
     public int getC() {
-        return c;
+        return this.c;
     }
 
     public int getD() {
-        return d;
+        return this.d;
     }
 }
+
